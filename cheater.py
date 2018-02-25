77256863b8e262f7196b92c8cc0c445d2cfc231b
Ishita Das <ishi0490@gmail.com>
2018-02-25 23:31:32 +0530

    comments added

--- cheater.py
+++ cheater.py
@@ -3,38 +3,28 @@ from Scrabble import wordlist, scores
 rack=input("enter the letters")
 f=open('sowpods.txt','r')
 r=f.read()
-#wordlist.append(r.split("\n"))
-# try:
-#     with open("sowpods.txt", "r") as f:
-#         for line in f:
-#             wordlist.append('\n')
-#
-# except EnvironmentError:
-#     print("Oops, I couldn't find sowpods.txt.")
-#     print("Am I in the right place?")
-#     exit(1)
 
 valid_words = []
 
-for word in wordlist:
-    valid = True
+for word in wordlist:                                   
+    valid = True                                       #initialize valid as true
     rack_letters = list(rack)
-    for letter in word:
+    for letter in word: 
         if letter not in rack_letters:
-            valid = False
+            valid = False                              #if a letter that is in the word but not in the given input thenproceed to next word 
             break
         else:
-            rack_letters.remove(letter)
+            rack_letters.remove(letter)                #if letter matches then remove that letter to handle dulplicate letter case
     total = 0
     if valid == True:
 
         for letter in word:
-            total = total + scores[letter]
+            total = total + scores[letter]             #if a word is found then calculate corresponding scores from the scores dictionary
         valid_words.append([total, word])
 
-valid_words.sort()
+valid_words.sort()                                     #sort according to scores from high to low
 
-for i in range(len(valid_words)-1,0,-1):
+for i in range(len(valid_words)-1,0,-1):               #for printing
     entry=valid_words[i]
     score = entry[0]
     word = entry[1]
